8b1043d0ff1bcd288189292e701eadbaebb8f656
Kuba Marchut <kuba.marchut@gmail.com>
2025-06-20 21:24:02 +0200

    implementation of simple version that works

--- supervisor/src/main/java/eu/deyanix/lorasupervisor/protocol/config/LoRaMode.java
+++ supervisor/src/main/java/eu/deyanix/lorasupervisor/protocol/config/LoRaMode.java
@@ -1,36 +1,6 @@
 package eu.deyanix.lorasupervisor.protocol.config;
 
 public enum LoRaMode {
-	SRC("Source", 0),
-	SNK("Sink", 1);
-
-	private final String fullName;
-	private final int value;
-
-	LoRaMode(String fullName, int value) {
-		this.fullName = fullName;
-		this.value = value;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
-	public int getValue() {
-		return value;
-	}
-
-	public static LoRaMode valueOf(int value) {
-		for (LoRaMode mode : values()) {
-			if (mode.getValue() == value) {
-				return mode;
-			}
-		}
-		return null;
-	}
-
-	@Override
-	public String toString() {
-		return fullName;
-	}
+	SRC,
+	SNK
 }
